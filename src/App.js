b6766c603e5eff76c8d18a7140101c4a1943ba5c
Robert Battaglia <r083rt164@gmail.com>
2019-08-14 15:08:27 -0400

    change selected category by selecting option from dropdown

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import CategorySelect from "./components/CategorySelect";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { questions: [] };
+    this.state = { category: null, questions: [] };
+    this.setCategory = this.setCategory.bind(this);
   }
 
   componentDidMount() {
     this.startGame(10, "general", "easy");
   }
 
-  startGame(qNum, cat, diff) {
+  setCategory(value) {
+    this.setState({ category: value });
+  }
+
+  startGame(qNum, category, difficulty) {
     axios
       .get("https://opentdb.com/api.php?amount=10&difficulty=easy")
       .then(({ data }) => {
@@ -34,7 +39,7 @@ class App extends Component {
         <header className="App-header" />
         <main>
           <h1>Trivia</h1>
-          <CategorySelect />
+          <CategorySelect handleSetCategory={this.setCategory} />
         </main>
       </div>
     );
--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -1,32 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class CategorySelect extends Component {
-  render() {
-    return (
-      <form>
-        <select>
-          <option value="9">General Knowledge</option>
-          <option value="10">Entertainment: Books</option>
-          <option value="11">Entertainment: Film</option>
-          <option value="12">Entertainment: Music</option>
-          <option value="13">Entertainment: Musicals & Theatres</option>
-          <option value="14">Entertainment: Television</option>
-          <option value="15">Entertainment: Video Games</option>
-          <option value="16">Entertainment: Board Games</option>
-          <option value="17">Science and Nature</option>
-          <option value="18">Science: Computers</option>
-          <option value="19">Science: Mathematics</option>
-          <option value="20">Mythology</option>
-          <option value="21">Sports</option>
-          <option value="22">Geography</option>
-          <option value="23">History</option>
-          <option value="24">Politics</option>
-          <option value="25">Art</option>
-          <option value="26">Celebrities</option>
-          <option value="27">Animals</option>
-          <option value="28">Vehicles</option>
-        </select>
-      </form>
-    );
+export default function CategorySelect(props) {
+  function setCategory(event) {
+    props.handleSetCategory(event.target.value);
   }
+  return (
+    <form>
+      <select onChange={setCategory}>
+        <option value="9">General Knowledge</option>
+        <option value="10">Entertainment: Books</option>
+        <option value="11">Entertainment: Film</option>
+        <option value="12">Entertainment: Music</option>
+        <option value="13">Entertainment: Musicals & Theatres</option>
+        <option value="14">Entertainment: Television</option>
+        <option value="15">Entertainment: Video Games</option>
+        <option value="16">Entertainment: Board Games</option>
+        <option value="17">Science and Nature</option>
+        <option value="18">Science: Computers</option>
+        <option value="19">Science: Mathematics</option>
+        <option value="20">Mythology</option>
+        <option value="21">Sports</option>
+        <option value="22">Geography</option>
+        <option value="23">History</option>
+        <option value="24">Politics</option>
+        <option value="25">Art</option>
+        <option value="26">Celebrities</option>
+        <option value="27">Animals</option>
+        <option value="28">Vehicles</option>
+      </select>
+    </form>
+  );
 }
