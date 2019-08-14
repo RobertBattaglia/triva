9f79297c5a7a49f611c7bdc137af9ab26d2821f7
Robert Battaglia <r083rt164@gmail.com>
2019-08-14 14:17:50 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <main>
+          <h1>Trivia</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
