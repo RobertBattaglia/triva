2e5b153861b747d2114f05317c26390fcf0039af
Robert Battaglia <r083rt164@gmail.com>
2019-08-14 14:38:21 -0400

    getting data from api when app did mount

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import axios from "axios";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { questions: [] };
+  }
+
+  componentDidMount() {
+    this.startGame(10, "general", "easy");
+  }
+
+  startGame(qNum, cat, diff) {
+    axios
+      .get("https://opentdb.com/api.php?amount=10&difficulty=easy")
+      .then(({ data }) => {
+        this.setState({ questions: data.results });
+      })
+      .then(() => {
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
